47d5d0b62a1eb76dd8f43d941fcf38ce26c4e469
Tarun-WDtops <tjparmar786@gmail.com>
2024-04-04 20:59:22 +0530

    portfolio added

--- portfolio.js
+++
@@ -1,7 +0,0 @@
-function on() {
-  if (document.getElementById("brightness").style.backgroundColor !== "black") {
-    document.getElementById("brightness").style.backgroundColor = "black";
-  } else {
-    document.getElementById("brightness").style.backgroundColor = "white";
-  }
-}
